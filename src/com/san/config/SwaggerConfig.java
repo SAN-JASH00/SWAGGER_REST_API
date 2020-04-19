821a07ead04ad40890523795e1a02d625aeb7426
San <scisntj@gmail.com>
2020-04-19 14:10:22 +0530

    Chng

--- src/com/san/config/SwaggerConfig.java
+++ src/com/san/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig extends HttpServlet{
 		beanConfig.setSchemes(schemes);
 		beanConfig.setScan(true);
 		beanConfig.setPrettyPrint(true);
-		beanConfig.setHost("java-swagger-restapi.herokuapp.com");
+		beanConfig.setHost("localhost:8080");
 	}
 	
 
